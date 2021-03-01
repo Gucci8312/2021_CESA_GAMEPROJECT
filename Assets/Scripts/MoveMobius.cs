f496afbb21ba37b22de5919461f20f49b2bbdf26
航起 山口 <kouki.0109721@au.com>
2021-03-01 16:20:40 +0900

    メビウスの輪の挙動改善

--- Assets/Scripts/MoveMobius.cs
+++ Assets/Scripts/MoveMobius.cs
@@ -9,7 +9,7 @@ public class MoveMobius : MonoBehaviour
     public float MovePower;                 // 移動力
     bool MoveFlg;                           // 移動判定用
     Rigidbody rb;                           // リジットボディ
-
+    public GameObject AttachMobius;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -24,24 +24,51 @@ public class MoveMobius : MonoBehaviour
             if (Input.GetKey(KeyCode.W))
             {
                 this.gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + MovePower, 0.0f);
+               // Debug.Log(MoveFlg);
             }
             if (Input.GetKey(KeyCode.S))
             {
                 this.gameObject.transform.position = new Vector3(transform.position.x, transform.position.y - MovePower, 0.0f);
+               // Debug.Log(MoveFlg);
             }
             if (Input.GetKey(KeyCode.A))
             {
                 this.gameObject.transform.position = new Vector3(transform.position.x - MovePower, transform.position.y, 0f);
+               // Debug.Log(MoveFlg);
             }
             if (Input.GetKey(KeyCode.D))
             {
-                this.gameObject.transform.position = new Vector3(transform.position.x + MovePower, transform.position.y, 0f);
+                 this.gameObject.transform.position = new Vector3(transform.position.x + MovePower, transform.position.y, 0f);
+               // rb.AddForce( MovePower, 0f, 0f);
+               // Debug.Log(MoveFlg);
             }
         }
     }
 
     // 衝突時
-    private void OnCollisionEnter(Collision other)
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    // メビウスの輪同士がぶつかったとき
+    //    if (other.gameObject.tag == "Mobius")
+    //    {
+    //        MoveFlg = false;
+    //        AttachMobius = other.gameObject;
+    //        Debug.Log("メビウスの輪同士がぶつかった");
+    //    }
+    //}
+
+    //// 離れた時
+    //private void OnCollisionExit(Collision other)
+    //{
+    //    // プレイヤーから離れた時
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        Debug.Log("プレイヤーから離れた");
+    //        //MoveFlg = false;
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
         // プレイヤーに当たった時
         if (other.gameObject.tag == "Player")
@@ -49,23 +76,29 @@ public class MoveMobius : MonoBehaviour
             MoveFlg = true;
             Debug.Log("プレイヤーに当たった");
         }
-
         // メビウスの輪同士がぶつかったとき
-        if (other.gameObject.tag == "Mobius")
+        else if (other.gameObject.tag == "Mobius")
         {
             MoveFlg = false;
+            AttachMobius = other.gameObject;
             Debug.Log("メビウスの輪同士がぶつかった");
         }
     }
-
-    // 離れた時
-    private void OnCollisionExit(Collision other)
+    private void OnTriggerExit(Collider other)
     {
-        // プレイヤーから離れた時
-        if (other.gameObject.tag == "Player")
+        if (MoveFlg)
         {
-            Debug.Log("プレイヤーから離れた");
-            MoveFlg = false;
+            if (other.gameObject.tag == "Player")
+            {
+                MoveFlg = false;
+                Debug.Log("プレイヤーから離れた");
+            }
         }
+    }
+
+
+    public GameObject GetAttacthMobius()
+    {
+        return AttachMobius;
     }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,6 +10,8 @@ public class PlayerMove : MonoBehaviour
     GameObject Mobius;                                                                              // メビウスの輪
     public float MovePower;                                                                         // 移動力
     float Angle;                                                                                    // 角度
+    float syahen;
+    Vector2 MobiusPos;
 
     void Start()
     {
@@ -22,53 +24,69 @@ public class PlayerMove : MonoBehaviour
     {
         if (Mobius != null)
         {
-            float hankei = Mobius.GetComponent<SphereCollider>().bounds.size.x / 2 +                // 円の半径を取得
-                GetComponent<SphereCollider>().bounds.size.x / 2;
-            Vector2 MobiusPos = Mobius.GetComponent<SphereCollider>().bounds.center;                // メビウスの輪の位置を取得
+            // float hankei = Mobius.GetComponent<SphereCollider>().bounds.size.x / 2 +                // 円の半径を取得
+            // GetComponent<SphereCollider>().bounds.size.x / 2;
+           // MobiusPos = Mobius.GetComponent<SphereCollider>().bounds.center;                // メビウスの輪の位置を取得
+            MobiusPos = Mobius.transform.position;                // メビウスの輪の位置を取得
 
             this.gameObject.transform.position = new Vector3(MobiusPos.x + Mathf.Cos(Angle) *       // 位置の指定
-                hankei, MobiusPos.y + Mathf.Sin(Angle) * hankei, 0.0f);
+                syahen, MobiusPos.y + Mathf.Sin(Angle) * syahen, 0.0f);
 
-            Angle -= Mathf.Deg2Rad * (MovePower/10);                                                //1度分ををラジアンに変換して加える
-
+            Angle -= Mathf.Deg2Rad * (MovePower / 10);                                                //1度分ををラジアンに変換して加える
+            
             // 360度を越えると初期化
             if (Angle > 360)
             {
-                Angle = 0;
+                Angle -= 360;
+            }
+            else if(Angle<0)
+            {
+                Angle += 360;
             }
             //Debug.Log(Angle);
         }
     }
 
     // 衝突時
-    //private void OnTriggerEnter(Collider other)
+   // private void OnTriggerEnter(Collider other)
     private void OnCollisionEnter(Collision other)
     {
         // 衝突したものがメビウスの輪の場合
         if (other.gameObject.tag == "Mobius")
         {
+           // if (other.gameObject)
+            if (other.gameObject.GetComponent<MoveMobius>().GetAttacthMobius())
+            {
+                Debug.Log("aaaaaaaaaaa");
+                if (other.gameObject.name == other.gameObject.GetComponent<MoveMobius>().GetAttacthMobius().name)
+                {
+                    Debug.Log("プレイヤーが隣接しているメビウスの輪に当たった");
+                }
+            }
             Mobius = other.gameObject;                                                              // 衝突したメビウスの輪を格納
             this.gameObject.transform.parent = other.gameObject.transform;                          // プレイヤーをメビウスの輪の子にする
             Vector2 PlayerPos = gameObject.transform.position;                                      // プレイヤーの位置を取得
-            Vector2 MobiusPos = Mobius.GetComponent<SphereCollider>().bounds.center;                // メビウスの輪の位置を取得
-            float syahen = Mobius.GetComponent<SphereCollider>().bounds.size.x / 2 +                // 斜辺を円のサイズから取得
-            GetComponent<SphereCollider>().bounds.size.x / 2;
+            MobiusPos = Mobius.transform.position;                                                  // メビウスの輪の位置を取得
+            syahen = Mobius.GetComponent<SphereCollider>().bounds.size.x / 2 +                  // 斜辺を円のサイズから取得
+            (GetComponent<SphereCollider>().bounds.size.x / 2*0.9f);
             float teihen = PlayerPos.x - MobiusPos.x;                                               // 底辺をプレイヤーとメビウスの輪から取得
             Angle = Mathf.Acos(teihen / syahen);                                                    // プレイヤーの現在のいる角度を取得
             rb.useGravity = false;                                                                  // 重力をOFFにする
+            Mobius.GetComponent<SphereCollider>().isTrigger = true;
             Debug.Log("メビウスの輪に当たった");
         }
     }
 
     // 離れた時
-    //private void OnTriggerExit(Collider other)
-    private void OnCollisionExit(Collision other)
+    private void OnTriggerExit(Collider other)
+    //private void OnCollisionExit(Collision other)
     {
         // 離れたものがメビウスの輪の場合
         if (other.gameObject.tag == "Mobius")
         {
-           //this.gameObject.transform.parent = null;                                               // 親を未選択にする
-            rb.useGravity = true;                                                                   // 重力をONにする
+            Mobius.GetComponent<SphereCollider>().isTrigger = false;
+            //this.gameObject.transform.parent = null;                                               // 親を未選択にする
+            // rb.useGravity = true;                                                                   // 重力をONにする
         }
     }
 
