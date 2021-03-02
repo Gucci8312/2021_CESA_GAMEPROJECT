d49b2fc45edb196c1212e6971e9715cfc1aabb27
航起 山口 <kouki.0109721@au.com>
2021-03-02 15:26:05 +0900

    2021/03/02

--- Assets/Scripts/MoveMobius.cs
+++ Assets/Scripts/MoveMobius.cs
@@ -9,10 +9,12 @@ public class MoveMobius : MonoBehaviour
     public float MovePower;                 // 移動力
     bool MoveFlg;                           // 移動判定用
     Rigidbody rb;                           // リジットボディ
+    GameObject player;
     public GameObject AttachMobius;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
@@ -21,84 +23,68 @@ public class MoveMobius : MonoBehaviour
         // プレイヤーが乗っているとき
         if (MoveFlg)
         {
+
             if (Input.GetKey(KeyCode.W))
             {
                 this.gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + MovePower, 0.0f);
-               // Debug.Log(MoveFlg);
+                // Debug.Log(MoveFlg);
             }
             if (Input.GetKey(KeyCode.S))
             {
                 this.gameObject.transform.position = new Vector3(transform.position.x, transform.position.y - MovePower, 0.0f);
-               // Debug.Log(MoveFlg);
+                // Debug.Log(MoveFlg);
             }
             if (Input.GetKey(KeyCode.A))
             {
                 this.gameObject.transform.position = new Vector3(transform.position.x - MovePower, transform.position.y, 0f);
-               // Debug.Log(MoveFlg);
+                // Debug.Log(MoveFlg);
             }
             if (Input.GetKey(KeyCode.D))
             {
-                 this.gameObject.transform.position = new Vector3(transform.position.x + MovePower, transform.position.y, 0f);
-               // rb.AddForce( MovePower, 0f, 0f);
-               // Debug.Log(MoveFlg);
+                this.gameObject.transform.position = new Vector3(transform.position.x + MovePower, transform.position.y, 0f);
+                // rb.AddForce( MovePower, 0f, 0f);
+                // Debug.Log(MoveFlg);
             }
         }
+
+
+        MoveFlg = false;
+
+        int num = player.GetComponent<PlayerMove>().GetNowMobiusNum();//プレイヤーオブジェクトから現在のメビウスの輪の数字取得
+
+        if (this.name == "Mobius (" + num + ")")//自分が対象のメビウスの輪なら
+        {
+            //Debug.Log("Mobius (" + num + "):対象のメビウスの輪");
+            MoveFlg = true;
+        }
+
     }
 
-    // 衝突時
+    //// 衝突時
     //private void OnCollisionEnter(Collision other)
     //{
     //    // メビウスの輪同士がぶつかったとき
     //    if (other.gameObject.tag == "Mobius")
     //    {
-    //        MoveFlg = false;
+    //        //MoveFlg = false;
     //        AttachMobius = other.gameObject;
     //        Debug.Log("メビウスの輪同士がぶつかった");
     //    }
     //}
 
-    //// 離れた時
+    ////// 離れた時
     //private void OnCollisionExit(Collision other)
     //{
-    //    // プレイヤーから離れた時
-    //    if (other.gameObject.tag == "Player")
+    //    if (other.gameObject.tag == "Mobius")
     //    {
-    //        Debug.Log("プレイヤーから離れた");
-    //        //MoveFlg = false;
+    //        //MoveFlg = true;
+    //        AttachMobius = other.gameObject;
+    //        Debug.Log("メビウスの輪同士がぶつかった");
     //    }
     //}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        // プレイヤーに当たった時
-        if (other.gameObject.tag == "Player")
-        {
-            MoveFlg = true;
-            Debug.Log("プレイヤーに当たった");
-        }
-        // メビウスの輪同士がぶつかったとき
-        else if (other.gameObject.tag == "Mobius")
-        {
-            MoveFlg = false;
-            AttachMobius = other.gameObject;
-            Debug.Log("メビウスの輪同士がぶつかった");
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (MoveFlg)
-        {
-            if (other.gameObject.tag == "Player")
-            {
-                MoveFlg = false;
-                Debug.Log("プレイヤーから離れた");
-            }
-        }
-    }
 
 
-    public GameObject GetAttacthMobius()
-    {
-        return AttachMobius;
-    }
+
+
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,87 +7,215 @@ public class PlayerMove : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody rb;                                                                                   // リジットボディ
-    GameObject Mobius;                                                                              // メビウスの輪
+    GameObject[] Mobius = new GameObject[4];                                                          // メビウスの輪
     public float MovePower;                                                                         // 移動力
     float Angle;                                                                                    // 角度
-    float syahen;
-    Vector2 MobiusPos;
+    public int NowMobius;                                                                           //現在のメビウスの添え字　
+    int SaveMobius;                                                                                 //１つ前にいたメビウスの添え字
+    public float InsideLength;                                                                      //内側に入ったときの位置を調整する値
+    public bool RotateLeftFlg;                                                                      //回転方向が左右のどちらかを判定　true:左　false:右
+    public bool InsideFlg;                                                                          //メビウスの輪の内側か外側かを判定　true:内側　false:外側
+    public float MobiusCollisonLength;                                                              //メビウスの輪同士で当たったときの判定用の長さ
+    int counter;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();                                                             // リジットボディを格納
+        //rb = GetComponent<Rigidbody>();                                                             // リジットボディを格納
+
+        for (int i = 0; i < 4; i++)
+        {
+            Mobius[i] = GameObject.Find("Mobius (" + i + ")");                                        //全てのメビウス取得
+        }
+        SaveMobius = -1;
     }
 
     // Update is called once per frame  
     //void FixedUpdate()
     void Update()
     {
-        if (Mobius != null)
+        if (InsideFlg)//メビウスの輪の内側
         {
-            // float hankei = Mobius.GetComponent<SphereCollider>().bounds.size.x / 2 +                // 円の半径を取得
-            // GetComponent<SphereCollider>().bounds.size.x / 2;
-           // MobiusPos = Mobius.GetComponent<SphereCollider>().bounds.center;                // メビウスの輪の位置を取得
-            MobiusPos = Mobius.transform.position;                // メビウスの輪の位置を取得
+            InsideLength = 22;
+        }
+        else//外側
+        {
+            InsideLength = 0;
+        }
 
-            this.gameObject.transform.position = new Vector3(MobiusPos.x + Mathf.Cos(Angle) *       // 位置の指定
-                syahen, MobiusPos.y + Mathf.Sin(Angle) * syahen, 0.0f);
+        if (Mobius[NowMobius] != null)
+        {
 
-            Angle -= Mathf.Deg2Rad * (MovePower / 10);                                                //1度分ををラジアンに変換して加える
-            
-            // 360度を越えると初期化
-            if (Angle > 360)
+            Vector2 MobiusPos = Mobius[NowMobius].GetComponent<SphereCollider>().bounds.center;                // メビウスの輪の位置を取得
+            float hankei = Mobius[NowMobius].GetComponent<SphereCollider>().bounds.size.x / 2 +                // 円の半径を取得
+                GetComponent<SphereCollider>().bounds.size.x / 2;
+
+            Vector2 PlayerPos = this.GetComponent<SphereCollider>().bounds.center;                             //プレイヤーの位置取得
+
+
+            Vector2 vecY = MobiusPos - PlayerPos;                                                              //プレイヤーの位置から対象のメビウスへのベクトルを求める(Y軸ベクトル)
+
+            Vector2 vecX;//(X軸ベクトル)
+            vecX.x = 0.0f * 0.0f - 1.0f * vecY.y;                                                              //Y軸とZ軸からX軸を求める
+            vecX.y = 1.0f * vecY.x - 0.0f * 0.0f;
+
+            float veclength = Mathf.Sqrt(vecY.x * vecY.x + vecY.y * vecY.y);                                   //Yベクトルの長さ計算
+            //単位ベクトルにする
+            vecY.x = vecY.x / veclength;
+            vecY.y = vecY.y / veclength;
+
+            veclength = Mathf.Sqrt(vecX.x * vecX.x + vecX.y * vecX.y);                                         //Xベクトルの長さ計算
+            //単位ベクトルにする
+            vecX.x = vecX.x / veclength;
+            vecX.y = vecX.y / veclength;
+
+            if (veclength > hankei - InsideLength)//対象のメビウスから離れている場合
             {
-                Angle -= 360;
+                this.gameObject.transform.position = new Vector3(this.transform.position.x + vecY.x * MovePower * Time.deltaTime, this.transform.position.y + vecY.y * MovePower * Time.deltaTime, 0);     //対象のメビウスに近づける
+
             }
-            else if(Angle<0)
+
+            if (RotateLeftFlg)//左回転
             {
-                Angle += 360;
+                this.gameObject.transform.position = new Vector3(this.transform.position.x - vecX.x * MovePower * Time.deltaTime, this.transform.position.y - vecX.y * MovePower * Time.deltaTime, 0);         //対象のメビウスの周りをまわる
+
             }
+            else//右回転
+            {
+                this.gameObject.transform.position = new Vector3(this.transform.position.x + vecX.x * MovePower * Time.deltaTime, this.transform.position.y + vecX.y * MovePower * Time.deltaTime, 0);         //対象のメビウスの周りをまわる
+
+            }
+
+
+            CollisonMobius();
+
+
+            if (counter > 0)
+            {
+                counter++;
+                if (counter > 50)
+                {
+                    SaveMobius = NowMobius;
+                    counter = 0;
+                }
+            }
+
+
+
+            //this.gameObject.transform.position = new Vector3(MobiusPos.x + Mathf.Cos(Angle) *       // 位置の指定
+            //    hankei, MobiusPos.y + Mathf.Sin(Angle) * hankei, 0.0f);
+
+            //Angle -= Mathf.Deg2Rad * (MovePower/10);                                                //1度分ををラジアンに変換して加える
+
+            //// 360度を越えると初期化
+            //if (Angle > 360)
+            //{
+            //    Angle = 0;
+            //}
             //Debug.Log(Angle);
         }
     }
 
-    // 衝突時
-   // private void OnTriggerEnter(Collider other)
-    private void OnCollisionEnter(Collision other)
+
+    private void CollisonMobius()//プレイヤーと対象のメビウスの輪以外の一番近いメビウスの輪との判定
     {
-        // 衝突したものがメビウスの輪の場合
-        if (other.gameObject.tag == "Mobius")
+
+        Vector2 NowMobiusPos = Mobius[NowMobius].GetComponent<SphereCollider>().bounds.center;                // 現在のメビウスの輪の位置を取得
+        Vector2 PlayerPos = this.GetComponent<SphereCollider>().bounds.center;                             //プレイヤーの位置取得
+
+        Vector2 NowVec = NowMobiusPos - PlayerPos;
+        float NowLength = Mathf.Sqrt(NowVec.x * NowVec.x + NowVec.y * NowVec.y);
+
+        float hankei = Mobius[NowMobius].GetComponent<SphereCollider>().bounds.size.x / 2 +                // 円の半径を取得
+                GetComponent<SphereCollider>().bounds.size.x / 2;
+
+        Vector2 NextMobiusPos;//次のメビウスの場所
+        Vector2 NextVec;
+        float NextLength = 0;
+        for (int i = 0; i < 4; i++)
         {
-           // if (other.gameObject)
-            if (other.gameObject.GetComponent<MoveMobius>().GetAttacthMobius())
+            if (i == NowMobius) continue;//現在のメビウスの位置は処理を飛ばす
+            if (i == SaveMobius) continue;
+
+            NextMobiusPos = Mobius[i].GetComponent<SphereCollider>().bounds.center;
+
+            Vector2 Vec = NextMobiusPos - NowMobiusPos;
+            float VecLength = Mathf.Sqrt(Vec.x * Vec.x + Vec.y * Vec.y);
+            if (hankei + hankei > VecLength)
             {
-                Debug.Log("aaaaaaaaaaa");
-                if (other.gameObject.name == other.gameObject.GetComponent<MoveMobius>().GetAttacthMobius().name)
+                Vec.x = Vec.x / VecLength;
+                Vec.y = Vec.y / VecLength;
+
+                Vector2 CollisonPos;//当たっている場所を計算(接点)
+                CollisonPos.x = NowMobiusPos.x + Vec.x * hankei;
+                CollisonPos.y = NowMobiusPos.y + Vec.y * hankei;
+
+                NextVec = CollisonPos - PlayerPos;
+                NextLength = Mathf.Sqrt(NextVec.x * NextVec.x + NextVec.y * NextVec.y);
+
+                //this.transform.position = new Vector3(CollisonPos.x, CollisonPos.y, 0);
+
+
+                if ((hankei/2)+(InsideLength/2) > NextLength)
                 {
-                    Debug.Log("プレイヤーが隣接しているメビウスの輪に当たった");
+                    SaveMobius = NowMobius;
+                    NowMobius = i;
+                    counter = 1;
+
+                    //内側と外側を反転させる
+                    if (InsideFlg)
+                    {
+                        InsideFlg = false;
+                    }
+                    else
+                    {
+                        InsideFlg = true;
+                    }
+
+                    //左右移動を反転させる
+                    if (RotateLeftFlg)
+                    {
+                        RotateLeftFlg = false;
+                    }
+                    else
+                    {
+                        RotateLeftFlg = true;
+                    }
+                    Debug.Log("メビウスの輪を切り替えた");
+
+                    break;
                 }
             }
-            Mobius = other.gameObject;                                                              // 衝突したメビウスの輪を格納
-            this.gameObject.transform.parent = other.gameObject.transform;                          // プレイヤーをメビウスの輪の子にする
-            Vector2 PlayerPos = gameObject.transform.position;                                      // プレイヤーの位置を取得
-            MobiusPos = Mobius.transform.position;                                                  // メビウスの輪の位置を取得
-            syahen = Mobius.GetComponent<SphereCollider>().bounds.size.x / 2 +                  // 斜辺を円のサイズから取得
-            (GetComponent<SphereCollider>().bounds.size.x / 2*0.9f);
-            float teihen = PlayerPos.x - MobiusPos.x;                                               // 底辺をプレイヤーとメビウスの輪から取得
-            Angle = Mathf.Acos(teihen / syahen);                                                    // プレイヤーの現在のいる角度を取得
-            rb.useGravity = false;                                                                  // 重力をOFFにする
-            Mobius.GetComponent<SphereCollider>().isTrigger = true;
-            Debug.Log("メビウスの輪に当たった");
+
+
+
+            if (MobiusCollisonLength + InsideLength > NowLength + NextLength)
+            {
+
+            }
         }
+
+
+    }
+
+
+    public int GetNowMobiusNum()
+    {
+        return NowMobius;
+    }
+
+    // 衝突時
+    // private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
+    {
+       
+        
     }
 
     // 離れた時
     private void OnTriggerExit(Collider other)
     //private void OnCollisionExit(Collision other)
     {
-        // 離れたものがメビウスの輪の場合
-        if (other.gameObject.tag == "Mobius")
-        {
-            Mobius.GetComponent<SphereCollider>().isTrigger = false;
-            //this.gameObject.transform.parent = null;                                               // 親を未選択にする
-            // rb.useGravity = true;                                                                   // 重力をONにする
-        }
+       
     }
 
 }
