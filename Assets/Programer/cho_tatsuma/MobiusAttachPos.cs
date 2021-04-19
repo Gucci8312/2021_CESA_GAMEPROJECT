f0ba8805535d24d2f20133a0b2324c41c277b796
HalOsaka_cho_tatsuma <tatsuma_cho@try.dokidoki-gw.com>
2021-04-19 15:04:33 +0900

    メビウスの輪完成した時の斜め移動対応？（未確認）

--- Assets/Programer/cho_tatsuma/MobiusAttachPos.cs
+++ Assets/Programer/cho_tatsuma/MobiusAttachPos.cs
@@ -7,10 +7,8 @@
 //プレイヤーが現在どのメビウスの輪に乗っているのかを随時確認し更新。
 //単体の輪が当たったことを確認したあと１で求めた輪のスクリプトから二つの輪の中点を抽出
 //メビウスの輪モデルの座標のを設定し、Meshをtrueに変更して出現させる。
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;       //Enumを使えるようにする
+using UnityEngine;
 
 public class MobiusAttachPos : MonoBehaviour
 {
@@ -26,6 +24,8 @@ public class MobiusAttachPos : MonoBehaviour
     Vector2 StickInput;                   //スティック入力時の値を取得用(-1～1)
     GameObject otherMobius;              //キャラのいる位置とは違った当たった邦のメビウスの格納変数
     bool m_mobiusCol;                   //メビウスがくっついたかどうかを一回だけ判定。
+
+    float before_degree;                //前回の回転角を保持
     // Start is called before the first frame update
     void Start()
     {
@@ -39,7 +39,7 @@ public class MobiusAttachPos : MonoBehaviour
         //すべてのメビウスの輪（単体）を取得
         for (int i = 0; i < m_mobius.Length; i++)
         {
-            m_mobius[i] = GameObject.Find("Mobius (" + i + ")");                                        
+            m_mobius[i] = GameObject.Find("Mobius (" + i + ")");
         }
 
         m_pCylinder = GameObject.Find("pCylinder2");
@@ -70,6 +70,7 @@ public class MobiusAttachPos : MonoBehaviour
                 MobiusChileMeshRenderOn(m_mobius[i]);
             }
             m_mobiusCol = false;
+            transform.Rotate(new Vector3(0, 0, -before_degree));
         }
 
         //メビウスの輪（単体）同士が当たったかどうかを取得
@@ -78,6 +79,7 @@ public class MobiusAttachPos : MonoBehaviour
             otherMobius = m_mobius[m_nowMobiusNo].GetComponent<MoveMobius>().GetColMobiusObj();
             MobiusChileMeshRenderOff(m_mobius[m_nowMobiusNo]);
             MobiusChileMeshRenderOff(otherMobius);
+            before_degree = MobiusRotateDegree();
             //当たった二つの中間点を取得→メビウスの輪の座標に設定するため
             Vector3 pos = m_mobius[m_nowMobiusNo].GetComponent<MoveMobius>().GetColPos();
             //メビウスの輪のモデルを表示
@@ -86,14 +88,12 @@ public class MobiusAttachPos : MonoBehaviour
             if (m_keyCode.ToString() == "S" || m_keyCode.ToString() == "W" || StickInput.y != 0)
             {
                 this.gameObject.GetComponent<Transform>().position = new Vector3(pos.x + 15, pos.y, pos.z);
-                if (myTransform.eulerAngles.z == 0)
-                    transform.Rotate(new Vector3(0, 0, 90));
+                transform.Rotate(new Vector3(0, 0, before_degree));
             }
             else if (m_keyCode.ToString() == "A" || m_keyCode.ToString() == "D" || StickInput.x != 0)
             {
                 this.gameObject.GetComponent<Transform>().position = new Vector3(pos.x, pos.y - 15, pos.z);
-                if (myTransform.eulerAngles.z == 90)
-                    transform.Rotate(new Vector3(0, 0, -90));
+                transform.Rotate(new Vector3(0, 0, before_degree));
             }
             m_mobiusCol = true;
         }
@@ -107,6 +107,7 @@ public class MobiusAttachPos : MonoBehaviour
         // 何かのキーが押下されたら
         if (Input.anyKeyDown)
         {
+            //すべてのキーから探し当てる
             foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
             {
                 if (Input.GetKeyDown(code))
@@ -117,6 +118,8 @@ public class MobiusAttachPos : MonoBehaviour
         }
     }
 
+    // @name   MobiusChileMeshRenderOff
+    // @brief  メビウスの輪の子供のメッシュレンダーをオフ（透明化処理）
     void MobiusChileMeshRenderOff(GameObject _gameObj)
     {
         //メビウスの輪（単体）のモデルのメッシュレンダーを取得（trueになっているもののみ）→子オブジェクト探索
@@ -131,6 +134,8 @@ public class MobiusAttachPos : MonoBehaviour
 
     }
 
+    // @name   MobiusChileMeshRenderOn
+    // @brief  メビウスの輪の子供のメッシュレンダーをオン（透明化解除処理）
     void MobiusChileMeshRenderOn(GameObject _gameObj)
     {
         //メビウスの輪（単体）のモデルのメッシュレンダーを取得（trueになっているもののみ）→子オブジェクト探索
@@ -142,4 +147,45 @@ public class MobiusAttachPos : MonoBehaviour
         }
 
     }
+
+    // @name   MobiusRotateDegree
+    // @brief  他のメビウスと当たった時の回転角の算出処理
+    // @return 回転角
+    float MobiusRotateDegree()
+    {
+        float degree;
+        float x, y, radian;
+
+        //三角形の底辺と高さを算出
+        x = m_mobius[m_nowMobiusNo].GetComponent<Transform>().position.x - otherMobius.GetComponent<Transform>().position.x;
+        y = m_mobius[m_nowMobiusNo].GetComponent<Transform>().position.y - otherMobius.GetComponent<Transform>().position.y;
+
+        //ラジアンを引き出す
+        radian = Mathf.Atan2(y, x);
+
+        //最終角度を求める
+        degree = radian * (180.0f / 3.141592f);
+
+        //90度、0度の補正
+        if ((degree >= 80f && degree <= 100f) || (degree >= 260f && degree <= 280f))
+        {
+            degree = 90f;
+        }
+        else if ((degree >= 170f && degree <= 190f))
+        {
+            degree = 0f;
+        }
+
+        //45度の補正
+        if ((degree >= 35f && degree <= 55f) || (degree >= 215f && degree <= 235f))
+        {
+            degree = 45f;
+        }
+        else if ((degree >= 125f && degree <= 145f) || (degree >= 305f && degree <= 325f))
+        {
+            degree = 135f;
+        }
+        return degree;
+    }
 }
+
