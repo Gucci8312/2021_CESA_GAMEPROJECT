f90b05d68aabe887ecb6ebcf06d952ec6c37bea3
航起 山口 <kouki.0109721@au.com>
2021-04-21 20:44:15 +0900

    チェックポイントの描画変更

---
+++ Assets/Programer/Gucci/UIMaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMaster : MonoBehaviour
+{
+    public GameObject[] JudgeUI;
+    CheckPointCount CountScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CountScript = GameObject.Find("CheckPointCount").GetComponent<CheckPointCount>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int idx=0;idx< CountScript.CheckPointNum;idx++)
+        {
+            JudgeUI[idx].SetActive(true);
+        }
+    }
+}
