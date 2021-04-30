e3bbbb8cce13eaf0fb96587d2845b86afdcc01cc
航起 山口 <kouki.0109721@au.com>
2021-04-30 22:16:11 +0900

    バグ修正

--- Assets/Programer/cho_tatsuma/Scripts/Rythm.cs
+++ Assets/Programer/cho_tatsuma/Scripts/Rythm.cs
@@ -62,8 +62,8 @@ public class Rythm : MonoBehaviour
         StartCoroutine("SuccessCheck");
 
         //音を再生・ループにする
-        stageBGM.Play();
-        stageBGM.loop = true;
+       // stageBGM.Play();
+       // stageBGM.loop = true;
 
         //フレームマネージャーからソースを取得
         m_frameManager = GameObject.Find("FrameManager");
