0058d7aa864bac46fe7bee8e316837267fd0b8a6
航起 山口 <kouki.0109721@au.com>
2021-05-02 20:48:43 +0900

    アニメーター追加　プレイヤー側で関数呼んでもらえれば使える

---
+++ Assets/Programer/Gucci/AnimaterControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimaterControl : MonoBehaviour
+{
+    private CharacterController CharaContorler;
+    private Animator Anim;
+    string NowState;
+    string OldState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharaContorler = GetComponent<CharacterController>();
+        Anim = GetComponent<Animator>();
+        NowState = "Walk";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            HipDrop();
+            OldState = NowState;
+            NowState = "HipDrop";
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Run();
+            OldState = NowState;
+            NowState = "Run";
+        }
+    }
+    public void HipDrop()
+    {
+        Anim.SetBool("HipDropFlg", true);
+    }
+
+    public void Walk()
+    {
+        Anim.SetBool("WalkFlg", true);
+    }
+
+    public void Run()
+    {
+        Anim.SetBool("RunFlg", true);
+    }
+
+    void StopHipDrop()
+    {
+        Anim.SetBool("HipDropFlg", false);
+        if(OldState == "Walk")
+        {
+            Walk();
+        }
+        else if(OldState == "Run")
+        {
+            Run();
+        }
+    }
+}
