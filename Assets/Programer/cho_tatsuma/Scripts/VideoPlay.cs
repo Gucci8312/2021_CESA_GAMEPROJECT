a21852b40410db5ad7882f57a7ef04238893ca99
HalOsaka_cho_tatsuma <tatsuma_cho@try.dokidoki-gw.com>
2021-05-07 09:55:28 +0900

    チュートリアル関連の実装（ビデオ）

---
+++ Assets/Programer/cho_tatsuma/Scripts/VideoPlay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoPlay : MonoBehaviour
+{
+    //　VideoPlayerコンポーネント
+    private VideoPlayer mPlayer;
+    public bool endVideo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPlayer = GetComponent<VideoPlayer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {       
+        if(mPlayer.time == mPlayer.length)
+        {
+            endVideo = true;
+        }
+    }
+}
