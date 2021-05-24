61be3f461c389c1edb2aff6cc220a9100a089a02
HalOsaka_cho_tatsuma <tatsuma_cho@try.dokidoki-gw.com>
2021-05-24 16:43:25 +0900

    PauseManager更新

--- Assets/Programer/cho_tatsuma/Scripts/PauseManager.cs
+++ Assets/Programer/cho_tatsuma/Scripts/PauseManager.cs
@@ -6,6 +6,7 @@ public class PauseManager : MonoBehaviour
 {
     static private GameObject m_player;
     static private GameObject[] m_enemy;
+    static private GameObject[] m_mobius;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,7 @@ public class PauseManager : MonoBehaviour
     {
         m_player = GameObject.FindGameObjectWithTag("Player");
         m_enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        m_mobius = GameObject.FindGameObjectsWithTag("Mobius");
     }
     // Update is called once per frame
     void Update()
@@ -28,6 +30,12 @@ public class PauseManager : MonoBehaviour
         {
             m_enemy[i].GetComponent<EnemyMove>().enabled = false;
         }
+        foreach(var obj in m_mobius)
+        {
+            obj.GetComponent<MoveLine>().enabled = false;
+            obj.GetComponent<MoveMobius>().enabled = false;
+            obj.GetComponent<EnemyMobius>().enabled = false;
+        }
     }
 
     static public void OffPause()
@@ -36,6 +44,12 @@ public class PauseManager : MonoBehaviour
         for (int i = 0; i < m_enemy.Length; i++)
         {
             m_enemy[i].GetComponent<EnemyMove>().enabled = true;
+        }
+        foreach (var obj in m_mobius)
+        {
+            obj.GetComponent<MoveLine>().enabled = true;
+            obj.GetComponent<MoveMobius>().enabled = true;
+            obj.GetComponent<EnemyMobius>().enabled = true;
         }
     }
 }
