392cb989174bb543fea76d613acf210c09932881
HalOsaka_cho_tatsuma <tatsuma_cho@try.dokidoki-gw.com>
2021-05-28 23:24:41 +0900

    たなかくん、まついくんが作ってくれたポーズ呼び出し

--- Assets/Programer/cho_tatsuma/Scripts/PauseManager.cs
+++ Assets/Programer/cho_tatsuma/Scripts/PauseManager.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
-    static private GameObject m_player;
-    static private GameObject[] m_enemy;
-    static private GameObject[] m_mobius;
+    //static private GameObject m_player;
+    //static private GameObject[] m_enemy;
+    //static private GameObject[] m_mobius;
     // Start is called before the first frame update
     void Start()
     {
     }
     static public void GameObjectFindInit()
     {
-        m_player = GameObject.FindGameObjectWithTag("Player");
-        m_enemy = GameObject.FindGameObjectsWithTag("Enemy");
-        m_mobius = GameObject.FindGameObjectsWithTag("Mobius");
+        //m_player = GameObject.FindGameObjectWithTag("Player");
+        //m_enemy = GameObject.FindGameObjectsWithTag("Enemy");
+        //m_mobius = GameObject.FindGameObjectsWithTag("Mobius");
     }
     // Update is called once per frame
     void Update()
@@ -26,36 +26,16 @@ public class PauseManager : MonoBehaviour
     static public void OnPause()
     {
         PlayerMove.PauseOn();
-        for(int i = 0; i < m_enemy.Length; i++)
-        {
-            m_enemy[i].GetComponent<EnemyMove>().enabled = false;
-        }
-        foreach(var obj in m_mobius)
-        {
-            if (obj.GetComponent<MoveLine>() != null)
-            {
-                obj.GetComponent<MoveLine>().enabled = false;
-            }
-            obj.GetComponent<MoveMobius>().enabled = false;
-            obj.GetComponent<EnemyMobius>().enabled = false;
-        }
+        EnemyMove.PauseOn();
+        MoveLine.StopFlagSet(true);
+        MoveMobius.StopFlagSet(true);
     }
 
     static public void OffPause()
     {
         PlayerMove.PauseOff();
-        for (int i = 0; i < m_enemy.Length; i++)
-        {
-            m_enemy[i].GetComponent<EnemyMove>().enabled = true;
-        }
-        foreach (var obj in m_mobius)
-        {
-            if (obj.GetComponent<MoveLine>() != null)
-            {
-                obj.GetComponent<MoveLine>().enabled = true;
-            }
-            obj.GetComponent<MoveMobius>().enabled = true;
-            obj.GetComponent<EnemyMobius>().enabled = true;
-        }
+        EnemyMove.PauseOff();
+        MoveLine.StopFlagSet(false);
+        MoveMobius.StopFlagSet(false);
     }
 }
