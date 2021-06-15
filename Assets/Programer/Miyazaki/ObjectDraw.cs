503a6c65486914e8e50dd0c72f651023c3f04dc3
Miyazakimasaya <72191798+Miyazakimasaya@users.noreply.github.com>
2021-06-15 23:11:30 +0900

    no message

---
+++ Assets/Programer/Miyazaki/ObjectDraw.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDraw : MonoBehaviour
+{
+
+	[System.Serializable]
+	struct Object_Score_Drow
+	{
+		public GameObject gameobject;
+		public int threshold;
+	}
+	[SerializeField] private List<Object_Score_Drow> Score_Obj;
+
+
+
+	void Object_Draw_Update(int nowsocre)
+    {
+		for (int i=0;i<Score_Obj.Count;i++)
+		{
+			if (nowsocre < Score_Obj[i].threshold)
+			{
+				Score_Obj[i].gameobject.SetActive(true);
+			}
+		}
+	}
+
+
+
+}
