62cbaed4b93c59be0a6f49ca637cd140e7400022
Miyazakimasaya <72191798+Miyazakimasaya@users.noreply.github.com>
2021-06-18 17:19:01 +0900

    当たり判定追加

---
+++ Assets/Programer/Miyazaki/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+	//int a;
+	//public GameObject[] game;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Debug.Log("すり抜けた！");
+		if (other.tag == "Wa")
+		{
+			//a++;
+			Debug.Log("当たっているわよ");
+			this.gameObject.SetActive(false);
+		}
+		
+	}
+
+}
