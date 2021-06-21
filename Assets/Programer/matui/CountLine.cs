0bb1a7ce11e64ad0914486786f0af66c7715a84f
Takumi0527 <takkun.ari.mana.527@gmail.com>
2021-06-21 17:28:11 +0900

    回数制限のやつ仮完成、もしカクカクするなら要修正

--- Assets/Programer/matui/CountLine.cs
+++ Assets/Programer/matui/CountLine.cs
@@ -10,25 +10,33 @@ public class CountLine : MonoBehaviour
         Live = 0,
         Dest,
     }
-    public LineStatus Status;
+    /*public*/ LineStatus Status;
 
     public float Count = 3;
     float InitCount;
     //bool isflag = true; //存在するかどうか（falseで消す）
     CrossLine Cl;
-    public float Wariai;
+    /*public*/ float Wariai;
 
     public float RecoveryMaxTime = 10;
     float time = 0;
 
     Renderer LineColor;//色のマテリアル
 
+    Vector3 InitPos;
+
+    MeshRenderer MeshRend;
+    BoxCollider BoxCol;
     // Start is called before the first frame update
     void Start()
     {
         InitCount = Count;
         Cl = this.GetComponent<CrossLine>();
         LineColor = this.GetComponent<Renderer>();
+        BoxCol = this.GetComponent<BoxCollider>();
+        MeshRend = this.GetComponent<MeshRenderer>();
+
+
         //LineColor.material.EnableKeyword("_EMISSION");
         CountState();
     }
@@ -40,11 +48,13 @@ public class CountLine : MonoBehaviour
         {
             case LineStatus.Live:
                 //DestroyLine();
+                LineShowOnOff(true);
                 CountDown();
                 CountState();
                 break;
 
             case LineStatus.Dest:
+                LineShowOnOff(false);
                 RecoveryTimer();
                 break;
         }
@@ -85,8 +95,9 @@ public class CountLine : MonoBehaviour
         else if (Wariai == 0)//０の場合は消す
         {
             //isflag = false;
-            //this.transform.position = new Vector3(2000, 0, 0);
-            this.gameObject.SetActive(false);
+            InitPos = this.transform.position;
+            this.transform.position = new Vector3(2000, 0, 0);
+            //LineShowOnOff(false);
             Status = LineStatus.Dest;
         }
     }
@@ -99,9 +110,19 @@ public class CountLine : MonoBehaviour
             time = 0;
             Count = InitCount;
             Status = LineStatus.Live;
+            this.transform.position = InitPos;
+            //LineShowOnOff(true);
+
         }
 
         time += Time.deltaTime;
+    }
+
+    private void LineShowOnOff(bool flag)
+    {
+        BoxCol.enabled = flag;
+        MeshRend.enabled = flag;
+        //this.gameObject.SetActive(flag);
     }
 
     //private void DestroyLine()
