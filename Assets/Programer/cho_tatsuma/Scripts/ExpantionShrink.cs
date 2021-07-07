b90a2db25d88911bdec06308dd5b63c506467593
HalOsaka_cho_tatsuma <tatsuma_cho@try.dokidoki-gw.com>
2021-07-07 21:13:19 +0900

    色々ぷっしゅ

--- Assets/Programer/cho_tatsuma/Scripts/ExpantionShrink.cs
+++ Assets/Programer/cho_tatsuma/Scripts/ExpantionShrink.cs
@@ -22,7 +22,7 @@ public class ExpantionShrink : MonoBehaviour
     float j = 1f;
     Transform myTransform;
     Vector3 initLocalScale;
-      
+    
     // Start is called before the first frame update
     void Start()
     {
